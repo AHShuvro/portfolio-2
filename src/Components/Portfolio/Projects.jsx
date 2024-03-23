0159729dc2fedbee57dd43f61c306ac19ac4e4a1
A H Shuvro <ahshuvro77777@gmail.com>
2024-03-23 12:45:55 +0600

    some responsive problem solved

--- src/Components/Portfolio/Projects.jsx
+++ src/Components/Portfolio/Projects.jsx
@@ -2,13 +2,13 @@
 const Projects = () => {
     return (
         <>
-            <div className="container py-32">
-                <div className="flex items-center justify-center gap-10 mb-12">
-                    <button className="hover:bg-[#6c757d] transition-all duration-500 text-[1.12rem] text-white py-3 px-8 rounded-[40rem] border-[0.1rem] hover:border-none border-[#6c757d] focus:bg-[#6c757d]" tabIndex="0">All</button>
-                    <button className="hover:bg-[#6c757d] transition-all duration-500 text-[1.12rem] text-white py-3 px-8 rounded-[40rem] border-[0.1rem] hover:border-none border-[#6c757d] focus:bg-[#6c757d]" tabIndex="0">HTML</button>
-                    <button className="hover:bg-[#6c757d] transition-all duration-500 text-[1.12rem] text-white py-3 px-8 rounded-[40rem] border-[0.1rem] hover:border-none border-[#6c757d] focus:bg-[#6c757d]" tabIndex="0">Wordpress</button>
-                    <button className="hover:bg-[#6c757d] transition-all duration-500 text-[1.12rem] text-white py-3 px-8 rounded-[40rem] border-[0.1rem] hover:border-none border-[#6c757d] focus:bg-[#6c757d]" tabIndex="0">SEO</button>
-                    <button className="hover:bg-[#6c757d] transition-all duration-500 text-[1.12rem] text-white py-3 px-8 rounded-[40rem] border-[0.1rem] hover:border-none border-[#6c757d] focus:bg-[#6c757d]" tabIndex="0">Shopify</button>
+            <div className="container py-8 md:py-16 lg:py-32">
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 items-center justify-center gap-6 px-2 md:px-0 mb-6 md:mb-8 lg:mb-12">
+                    <button className="hover:bg-[#6c757d] transition-all duration-500 text-[1rem] md:text-[1.12rem] text-white py-1 lg:py-3 px-1 md:px-3 lg:px-4 rounded-[40rem] border-[0.1rem] hover:border-none border-[#6c757d] focus:bg-[#6c757d]" tabIndex="0">All</button>
+                    <button className="hover:bg-[#6c757d] transition-all duration-500 text-[1rem] md:text-[1.12rem] text-white py-1 lg:py-3 px-1 md:px-3 lg:px-4 rounded-[40rem] border-[0.1rem] hover:border-none border-[#6c757d] focus:bg-[#6c757d]" tabIndex="0">HTML</button>
+                    <button className="hover:bg-[#6c757d] transition-all duration-500 text-[1rem] md:text-[1.12rem] text-white py-1 lg:py-3 px-1 md:px-3 lg:px-4 rounded-[40rem] border-[0.1rem] hover:border-none border-[#6c757d] focus:bg-[#6c757d]" tabIndex="0">Wordpress</button>
+                    <button className="hover:bg-[#6c757d] transition-all duration-500 text-[1rem] md:text-[1.12rem] text-white py-1 lg:py-3 px-1 md:px-3 lg:px-4 rounded-[40rem] border-[0.1rem] hover:border-none border-[#6c757d] focus:bg-[#6c757d]" tabIndex="0">SEO</button>
+                    <button className="hover:bg-[#6c757d] transition-all duration-500 text-[1rem] md:text-[1.12rem] text-white py-1 lg:py-3 px-1 md:px-3 lg:px-4 rounded-[40rem] border-[0.1rem] hover:border-none border-[#6c757d] focus:bg-[#6c757d]" tabIndex="0">Shopify</button>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
                     <div className="">
