befad1e36d231c573abaffecd4f829008cf380ad
A H Shuvro <ahshuvro77777@gmail.com>
2024-03-23 11:44:41 +0600

    some responsive isues fixed

--- src/Components/BlogPost/BlogPost.jsx
+++ src/Components/BlogPost/BlogPost.jsx
@@ -18,7 +18,7 @@ const BlogPost = () => {
                         blogs.slice(0, 3).map((item, idx) => (
                             <Link key={idx} to={'/blog'}><div className="flex flex-col lg:flex-row items-center justify-between p-[3.12rem] bg-[#252734]">
                                 <img src="assets/img/blog-feed-img-1.webp" alt="" />
-                                <div>
+                                <div className="mt-6 mb-4">
                                     <p className="text-[1rem] text-white font-poppins">Business - <span className=" text-[#858792]">07 February, 2021</span></p>
                                     <h3 className="text-lg lg:text-2xl leading-10 text-white font-medium font-poppins mt-6">{`Don't`} wait until you officially started <br />business to line these up.</h3>
                                 </div>
