5defef94436cedde2a4eeb3404a478d131c2ae79
A H Shuvro <ahshuvro77777@gmail.com>
2024-03-23 11:54:56 +0600

    some issues fiexd

--- src/Components/BlogPost/BlogPost.jsx
+++ src/Components/BlogPost/BlogPost.jsx
@@ -18,9 +18,9 @@ const BlogPost = () => {
                         blogs.slice(0, 3).map((item, idx) => (
                             <Link key={idx} to={'/blog'}><div className="flex flex-col lg:flex-row items-center justify-between p-[3.12rem] bg-[#252734]">
                                 <img src="assets/img/blog-feed-img-1.webp" alt="" />
-                                <div className="mt-6 mb-4">
+                                <div className="mt-6 mb-4 text-center md:text-balance">
                                     <p className="text-[1rem] text-white font-poppins">Business - <span className=" text-[#858792]">07 February, 2021</span></p>
-                                    <h3 className="text-lg lg:text-2xl leading-10 text-white font-medium font-poppins mt-6">{`Don't`} wait until you officially started <br />business to line these up.</h3>
+                                    <h3 className="text-lg lg:text-2xl leading-10 text-white font-medium font-poppins mt-6">{`Don't`} wait until you officially started <br className="hidden md:block" />business to line these up.</h3>
                                 </div>
                                 <Link to={`/blogDetails/${item.id}`}><button className="hover:bg-[#2A2C39] transition-all duration-500 text-[1rem] text-white h-16 px-10 rounded-[40rem] border-[0.062rem] border-[#3b3d49] font-poppins">Read More {`>>`}</button></Link>
                             </div></Link>
